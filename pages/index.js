5470cadf82eaf2c880466d2fbc0aa363002784d0
Renata Magno <renatamagno@live.com>
2023-10-01 22:42:38 +0000

    add pages

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>A evolução é uma espiral, há fases boas e ruins. Hoje eu apresento mais um pedacinho da minha evolução de muitos dias de estudo, dias bons e dias ruins. A sua evolução também terá dias bons e dias ruins, mas lembre-se que todo esse espiral é o que faz com que a recompensa seja prazerosa.</h1>
+}
+
+export default Home;
